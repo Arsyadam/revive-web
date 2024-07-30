7c0f5a438f997997b9403d706bad0efef85503ae
Arsyadam <work@arsyadam.id>
2024-07-30 15:16:13 +0700

    update pdf

---
+++ src/app/vpc/page.tsx
@@ -0,0 +1,22 @@
+// Import the main component
+"use client"
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+// Import the styles
+import "@react-pdf-viewer/core/lib/styles/index.css";
+
+export default function Home() {
+  return (
+    <>
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+        <div
+          style={{
+            border: "1px solid rgba(0, 0, 0, 0.3)",
+            height: "750px",
+          }}
+        >
+          <Viewer fileUrl="/docs/vpc.pdf" />
+        </div>
+      </Worker>
+    </>
+  );
+}
